96f089a2a2f2b6955ffd18d1ece574ccdb0319c6
rnessiuh23 <fgarciasanluiso5@gmail.com>
2023-03-26 13:28:01 -0600

    v3

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/escuela"
 
-export const PORT = process.env.PORT || 3000;
+export const PUERTO = process.env.PORT || 3000;
