df0805e05d79b3cb28c42806a38795f7fae4406f
rnessiuh23 <fgarciasanluiso5@gmail.com>
2023-03-26 10:25:50 -0600

    v3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app"; //Equivale al codigo existente en app.js
 import "./database";
 import { PORT } from "./config";
-const port = process.env.PORT || PORT
-app.listen(port);
-console.log("Servidor en puerto", port);
+
+app.listen(PORT);
+console.log("Servidor en puerto", PORT);
